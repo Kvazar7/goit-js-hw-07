355449d9ee7bc332179da0d9e461c2e100ce20ae
Kvazar7 <Kometa737@gmail.com>
2023-01-05 01:41:02 +0200

    begin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galeryWrap = document.querySelector('.gallery');
+const creatingAnImageGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`})
+    .join('');
+
+galeryWrap.insertAdjacentHTML('afterbegin', creatingAnImageGallery)
+galeryWrap.addEventListener('click', doNotFollowTheLink);
+
+function doNotFollowTheLink(e) {
+    e.preventDefault();
+}
+
+document.querySelector('.gallery__item.gallery__link').onclick = () => {
+
+	basicLightbox.create(`
+		${data-source.innerHTML()}
+	`).show()
+
+}
+
 console.log(galleryItems);
