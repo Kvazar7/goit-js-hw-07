a445609c10c69b6a8fc188dc7ce21434a1328f79
Kvazar7 <Kometa737@gmail.com>
2023-01-05 22:03:09 +0200

    1st exercise

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,9 +2,10 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galeryWrap = document.querySelector('.gallery');
+
 const creatingAnImageGallery = galleryItems.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
+          <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
             src="${preview}"
@@ -15,16 +16,26 @@ const creatingAnImageGallery = galleryItems.map(({ preview, original, descriptio
       </li>`})
     .join('');
 
-galeryWrap.insertAdjacentHTML('afterbegin', creatingAnImageGallery)
-galeryWrap.addEventListener('click', doNotFollowTheLink);
+
+galeryWrap.insertAdjacentHTML('afterbegin', creatingAnImageGallery);
+
+galeryWrap.addEventListener('click', enlargeImg);
 
 function doNotFollowTheLink(e) {
     e.preventDefault();
-}
+};
 
-const showBigImg =	basicLightbox.create(`
-		<img src = "${evt.target.dataset.source}">
-	`)
-    showBigImg.show()
+function enlargeImg(e) {
+  doNotFollowTheLink(e)
+
+  // if (e.target.nodeName !== "IMG") {
+  //   return;
+  // }
+
+  const showBigImg = basicLightbox.create(`
+		<img src="${e.target.dataset.source}" width="800" height="600">
+	`);
+  showBigImg.show();
+};
 
 console.log(galleryItems);
